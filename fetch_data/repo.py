989a886334b25460b67e351b6fc3cda7cb189380
lanecatm <lanecatm@sjtu.edu.cn>
2016-11-01 21:39:16 +0800

    repo 接口

--- fetch_data/repo.py
+++ fetch_data/repo.py
@@ -1,7 +1,7 @@
 # -*- encoding:utf-8 -*-
 
 # ==============================================================================
-# Filename: Repo.py
+# Filename: repo.py
 # Author: Xiaofu Huang
 # E-mail: lanecatm@sjtu.edu.cn
 # Last modified: 2016-10-26 17:05
@@ -27,6 +27,9 @@ class repo:
         """
         self._connection.close()
 
+
+    # 插入抓到的数据
+    # @param infoArr [x1, x1, ..., x32]
     def insert_data(self, infoArr):
         statement = "INSERT INTO main.history_stock_info VALUES(NULL,"
         for i in range(0, 31):
@@ -36,4 +39,18 @@ class repo:
         cursor = self._connection.execute(statement)
         self._connection.commit()
         cursor.close()
+    
+    # 获取股票的成交数量
+    # @param startDate dateTime 从哪一天开始
+    # @param endDate dateTime 到哪一天结束
+    # @param startTime time 每一天中的开始时间(包含这一刻)
+    # @param endTime time 每一天中的结束时间(不包含这一刻)
+    # @return [[amount1, amount2, ..., amountn]
+    #          [amount1, amount2, ..., amountn]
+    #          ...                             
+    #         ]
+    def get_amount(self, startDate, endDate, startTime, endTime):
+        
 
+        
+        return 
