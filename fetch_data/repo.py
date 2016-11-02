45521305ce57250629a73e2c371e8360aa9e1090
黄晓芙 <xfhuang@huangxiaofudeMacBook-Pro.local>
2016-11-02 23:10:29 +0800

    change repo.py get_amount function

--- fetch_data/repo.py
+++ fetch_data/repo.py
@@ -9,6 +9,8 @@
 # ==============================================================================
 
 import sqlite3
+import time
+import numpy as np
 class repo:
     def __init__(self, file_path):
         # here should be abs path when connect db files
@@ -45,17 +47,22 @@ class repo:
     # @param endDate dateTime 到哪一天结束(较小的天数，不包含)
     # @param startTime time 每一天中的开始时间(包含这一刻)
     # @param endTime time 每一天中的结束时间(不包含这一刻)
-    # @return [[amount1, amount2, ..., amountn],
+    # @return  np.array
+    #          [[amount1, amount2, ..., amountn],
     #          [amount1, amount2, ..., amountn],
     #          ...                             
-    #         ]
+    #          ]
     # For Example
     # startDate = 10.5 endDate = 10.2 startTime = 13:00 endTime = 15:00
     # return [[10.5 13:00 amount, 10.5 13:01 amount, 10.5 13:02 amount, ..., 10.5 14:59 amount],
     #         [10.4 13:00 amount, 10.4 13:01 amount, 10.4 13:02 amount, ..., 10.4 14:59 amount],
     #         [10.3 13:00 amount, 10.3 13:01 amount, 10.3 13:02 amount, ..., 10.3 14:59 amount]]
     def get_amount(self, startDate, endDate, startTime, endTime):
-        
-
-        
-        return 
+        statement = "SELECT SUCCNUM FROM main.history_stock_info WHERE DATETIME(NOWDATE) > DATETIME(\"" + time.strftime("%Y-%m-%d", endDate) +"\") AND DATETIME(NOWDATE) <= DATETIME(\"" + time.strftime("%Y-%m-%d", startDate)\
+                +"\") AND TIME(NOWTIME) < TIME(\"" + time.strftime("%H:%M:%S", endTime) + "\") AND TIME(NOWTIME) >= TIME(\"" + time.strftime("%H:%M:%S", startTime) + "\")"
+        cursor = self._connection.execute(statement)
+        self._connection.commit()
+        data = cursor.fetchall()
+        cursor.close()
+        data = np.array(data)
+        return data
