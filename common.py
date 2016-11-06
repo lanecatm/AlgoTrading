2d179c4f040a016b08807b394aed1535dfbe39d8
jimusichen <jimusichen@gmail.com>
2016-11-06 13:27:29 +0800

    add class tradingunit

--- common.py
+++ common.py
@@ -14,7 +14,7 @@ class clientOrder:
     def __init__(self, *args):
         # 默认参数
         if len(args) != 8:
-            return None
+            raise BaseException("Class clientOrder: the number of parameters must be 8!")
         self.orderID = args[0]
         self.stockID = args[1]
         self.startTime = args[2]
@@ -24,6 +24,7 @@ class clientOrder:
         self.tradingAlgChoice = args[6]
         # 买或卖
         self.tradingType = args[7]
+
 
     def setOrderID(self, orderID):
         self.orderID = orderID
@@ -47,7 +48,7 @@ class orderResult:
     # 初始化 orderResult(orderID, stockID)
     def __init__(self, *args):
         if len(args) != 2:
-            return None
+            raise BaseException("Class orderResult: the number of parameters must be 2!")
         self.orderID = args[0]
         self.stockID = args[1]
         resultList = []
@@ -55,6 +56,17 @@ class orderResult:
         self.orderID = orderID
     def setStockID(self, stockID):
         self.stockID = stockID
+
+class tradingUnit:
+    # 初始化 tradingUnit(time, stockID, amount)
+    def __init__(self, *args):
+        if len(args) != 3:
+            raise BaseException("Class tradingUnit: the number of parameters must be 3!")
+        self.time = args[0]
+        self.stockID = args[1]
+        self.amount = args[2]
+        self.isSuccess = False;
+        self.price = -1;
 
 if __name__ == '__main__':
     pass
