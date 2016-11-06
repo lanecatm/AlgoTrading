c4bc1bd104c39a92a73e58141ee3ae4c87a1c9de
Peachying <1120025860@qq.com>
2016-11-06 22:06:07 +0800

    Add files via upload
    after unittest of VWAP

---
+++ LinearVWAPQuantAnalysisUnitTest.py
@@ -0,0 +1,20 @@
+# -*- encoding:utf-8 -*-
+
+
+import time
+import datetime
+from LinearVWAPQuantAnalysis import LinearVWAPQuantAnalysis
+
+class repoTest:
+    def get_amount( self, startDate, endDate, startTime, endTime):
+        return [[1,2,3,4]]*20
+	#print startDate, endDate, startTime, endTime
+
+if __name__ == '__main__':
+    repoEngine = repoTest()
+
+    LinearVWAPQuantAnalysis = LinearVWAPQuantAnalysis(repoEngine)
+    startDate=datetime.datetime.strptime("2016-11-01 10:00:00" , "%Y-%m-%d %H:%M:%S")
+    endDate = datetime.datetime.strptime("2016-10-10 13:00:00", "%Y-%m-%d %H:%M:%S")
+
+    LinearVWAPQuantAnalysis.getRecommendOrderWeight(startDate, endDate, 1)
