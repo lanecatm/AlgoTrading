14de917d06ad7aa44482d43a1b659a4c3d62d3eb
weiyi <weiyisjtu@126.com>
2016-11-07 14:59:26 +0800

    add status

--- common/clientOrder.py
+++ common/clientOrder.py
@@ -1,6 +1,6 @@
 class clientOrder(object):
 	"""docstring for clientOrder"""
-	def __init__(self, orderId, stockId, startTime, endTime, stockAmount, buysell, algChoice, timeInterval):
+	def __init__(self, orderId, stockId, startTime, endTime, stockAmount, buysell, algChoice, timeInterval, status):
 		#super(clientOrder, self).__init__()
 		#self.arg = arg
 		self.orderId = orderId
@@ -11,3 +11,4 @@ class clientOrder(object):
 		self.algChoice = algChoice
 		self.buysell = buysell
 		self.timeInterval = timeInterval
+                self.status = status # 0 places, 1 sent, 2 completed
