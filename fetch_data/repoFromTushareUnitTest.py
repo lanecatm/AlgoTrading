57afa096a365ed5ca045005602b597639e4c8600
lanecatm <lanecatm@sjtu.edu.cn>
2016-11-09 01:53:32 +0800

    change repoFromTushare

--- fetch_data/repoFromTushareUnitTest.py
+++ fetch_data/repoFromTushareUnitTest.py
@@ -8,9 +8,9 @@
 # Description: repo单元测试
 # ==============================================================================
 from repoFromTushare import repoFromTushare
-import time
+from datetime import datetime
 
 if __name__ == '__main__':
     repoEngine = repoFromTushare()
-    repoEngine.get_amount(time.strptime("2016-10-24", "%Y-%m-%d"),time.strptime("2016-10-20", "%Y-%m-%d"),time.strptime("13:00:00", "%H:%M:%S"),time.strptime("14:59:00", "%H:%M:%S"))
+    repoEngine.get_amount(datetime.strptime("2016-10-12", "%Y-%m-%d"),datetime.strptime("2016-10-10", "%Y-%m-%d"),datetime.strptime("13:30:00", "%H:%M:%S"),datetime.strptime("14:59:00", "%H:%M:%S"))
 
