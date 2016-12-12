b1531fec15cc97c45b8c357849dbea02c506b56e
xfhuang <lanecatm@sjtu.edu.cn>
2016-12-12 23:11:19 +0800

    fix too large again

--- cli/placeorder.sh
+++ cli/placeorder.sh
@@ -1,1 +1,2 @@
-python cli.py placeorder --stockid 600000 --amount 10000 --start 2016-10-14 --starttime 10:00:00 --end 2016-10-14 --endtime 11:50:00 --alg vwap --yes
+echo "python cli.py placeorder --stockid 600000 --amount 10000 --start 2016-10-14 --starttime 10:00:00 --end 2016-10-14 --endtime 11:50:00 --alg vwap --yes"
+python cli.py placeorder --stockid 600000 --amount 10000 --start 2016-10-14 --starttime 10:00:00 --end 2016-10-14 --endtime 11:50:00 --alg twap --yes
