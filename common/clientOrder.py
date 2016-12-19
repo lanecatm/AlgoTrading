9595f6ca3f8d2edc7e364ee036e54ee881313b94
weiyi <weiyisjtu@126.com>
2016-12-20 01:09:50 +0800

    add completed, remove time interval

--- common/clientOrder.py
+++ common/clientOrder.py
@@ -1,14 +1,15 @@
 class clientOrder(object):
-	"""docstring for clientOrder"""
-	def __init__(self, orderId, stockId, startTime, endTime, stockAmount, buysell, algChoice, timeInterval, status):
+	# def __init__(self, orderId, stockId, startTime, endTime, stockAmount, buysell, algChoice, timeInterval, completed, status):
+	def __init__(self, orderId, stockId, startTime, endTime, stockAmount, buysell, algChoice, completed, status):
 		#super(clientOrder, self).__init__()
-		#self.arg = arg
+		#super(clientOrder, self).__init__()
 		self.orderId = orderId
 		self.stockId = stockId
 		self.startTime = startTime
 		self.endTime = endTime
 		self.stockAmount = stockAmount
-		self.algChoice = algChoice
 		self.buysell = buysell
-		self.timeInterval = timeInterval
+		self.algChoice = algChoice
+		# self.timeInterval = timeInterval
+                self.completed = completed # number of shares that have been completed.
 		self.status = status # 0 places, 1 sent, 2 completed
