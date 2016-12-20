f8ba5d45597d6ee36a6aca064d67c1b53de64134
lanecatm <lanecatm@sjtu.edu.cn>
2016-12-20 22:35:35 +0800

    change data into mysql #3

---
+++ fetch_data/changeDataFromSqliteToMysql.py
@@ -0,0 +1,19 @@
+# -*- encoding:utf-8 -*-
+
+# ==============================================================================
+# Filename: changeDataFromSqliteToMysql.py
+# Author: Xiaofu Huang
+# E-mail: lanecatm@sjtu.edu.cn
+# Last modified: 2016-12-20 13:34
+# Description: change data from sqlite to mysql
+# ==============================================================================
+
+import sys, os
+import sqlite3
+from repo import repo
+sys.path.append("../tool")
+from Log import Log
+
+
+sqliteRepo = repo(True, True, "again_2.db", "algotrading", "12345678", None)
+sqliteRepo.change_data_into_mysql()
