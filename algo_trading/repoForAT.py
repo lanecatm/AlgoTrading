516400de8183a975228de18f826a3ca9a01b1b61
jimusichen <jimusichen@gmail.com>
2016-12-22 20:11:40 +0800

    indented problem

--- algo_trading/repoForAT.py
+++ algo_trading/repoForAT.py
@@ -11,28 +11,28 @@ from Log import Log
 class repoForAT:
     def __init__(self, user, password, ip, isOpenLog = True):
         self.log = Log(isOpenLog)
-	if ip==None:
-	    self._mysql_db = MySQLdb.connect("localhost", user, password, "algotradingDB")
+    if ip==None:
+        self._mysql_db = MySQLdb.connect("localhost", user, password, "algotradingDB")
         else:
             self._mysql_db = MySQLdb.connect(ip, user, password, "algotradingDB")
         self._mysql_cursor = self._mysql_db.cursor()
 
     def __del__(self):
-    	self._mysql_cursor.close()
-    	self._mysql_db.close()
+        self._mysql_cursor.close()
+        self._mysql_db.close()
 
     def extract_orders(self):
-	sql = "SELECT * FROM algotradingDB.client_orders WHERE COMPLETEDAMOUNT < STOCKAMOUNT"
-	self.log.info("get_final statement : " + sql)
-	self._mysql_cursor.execute(sql)
-	data = self._mysql_cursor.fetchall()
-	return data
+        sql = "SELECT * FROM algotradingDB.client_orders WHERE COMPLETEDAMOUNT < STOCKAMOUNT"
+        self.log.info("get_final statement : " + sql)
+        self._mysql_cursor.execute(sql)
+        data = self._mysql_cursor.fetchall()
+        return data
 
     # quantanalysis MUST be string
     def save_qa_result(self, orderId, quantanalysis):
         sql = "UPDATE algotradingDB.client_orders SET QUANTANALYSIS = '" + quantanalysis + "' WHERE ID = " + str(orderId)
-	self.log.info("get_final statement : " + sql)
-	self._mysql_cursor.execute(sql)
+        self.log.info("get_final statement : " + sql)
+        self._mysql_cursor.execute(sql)
 
     # update the completed amount (and status) after trade
     def post_trade(self, orderId, completed_amount):
