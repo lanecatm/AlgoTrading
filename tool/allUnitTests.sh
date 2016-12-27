e9002535abc23418be53410f5f20f77d5a076a45
weiyik <weiyi1994@sjtu.edu.cn>
2016-12-27 20:06:54 +0800

    run all unit tests

---
+++ tool/allUnitTests.sh
@@ -0,0 +1,14 @@
+cd ../algo_trading/
+python algoTradingUnitTest.py
+python repoForATUnitTest.py
+cd ../common/
+python MarketDataUnittest.py
+cd ../pool/
+python poolFromSinaApiUnitTest.py
+python tradingRecordSaverUnitTest.py 
+cd ../quant_analysis/
+python LinearVWAPQuantAnalysisUnitTest.py
+python TWAPQuantAnalysisUnitTest.py
+python VWAPQuantAnalysisUnitTest.py
+python quantAnalysisBaseUnitTest.py
+cd ..
